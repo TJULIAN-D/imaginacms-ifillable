2a5f3aeedfa85b5bbea0a5b972f48383e1c246c1
msolanogithub <msolano@imaginacolombia.com>
2024-03-11 13:38:36 -0500

    added `pivot` to ignored fields

--- helpers.php
+++ helpers.php
@@ -11,7 +11,7 @@ if (!function_exists('getIgnoredFields')) {
         'updated_at', 'updated_by',
         'deleted_at', 'deleted_by',
         'media_files', 'medias_single',
-        'medias_multi'
+        'medias_multi', 'pivot'
       ],
       //Ignore any available locale as field
       array_keys(config('available-locales'))
